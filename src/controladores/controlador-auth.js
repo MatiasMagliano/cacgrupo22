bb471fe5acf0e0b2535d5f5ad06eae159aadd2a1
MatiasMagliano <matias.magliano@mi.unc.edu.ar>
2024-06-28 22:27:14 -0300

    Agrega formulario de nuevo producto

--- src/controladores/controlador-auth.js
+++ src/controladores/controlador-auth.js
@@ -1,13 +1,20 @@
-import sharp from 'sharp';
 import { pool, VIEWS, cookie, appdevsatus } from '../config/app-config.js';
+import { validationResult } from 'express-validator';
+import fs from 'fs';
 import path from 'path';
+import sharp from 'sharp';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import { fileURLToPath } from 'url';
 
 // modelos necesarios
 import { Usuario } from "../modelos/user.js"
-import { name } from 'ejs';
 const modeloUsuario = new Usuario();
+import { Producto } from "../modelos/producto.js"
+const modeloProducto = new Producto();
+import { Categoria } from "../modelos/categoria.js"
+const modeloCategoria = new Categoria();
 
 export class ControladorAuth {
 
@@ -17,15 +24,15 @@ export class ControladorAuth {
         try {
             // Buscar el usuario por su nombre de usuario
             const usuario = await modeloUsuario.findByUsername(username);
-
+            
             // Si el usuario no existe
-            if (!usuario) {
+            if (usuario[0].length > 0) {
                 req.flash('error_msg', '¡Nombre de usuario o contraseña incorrectos!');
                 return res.redirect('/login');
             }
 
             // Verificar la contraseña
-            const passwordValida = await modeloUsuario.verificarContrasenia(password, usuario.password);
+            const passwordValida = await modeloUsuario.verifyPassword(password, usuario[0].password);
             if (!passwordValida) {
                 req.flash('error_msg', '¡Nombre de usuario o contraseña incorrectos!');
                 return res.redirect('/login');
@@ -33,7 +40,7 @@ export class ControladorAuth {
 
             // Generar el token JWT
             const token = jwt.sign(
-                { id: usuario.id, username: usuario.username },
+                { id: usuario[0].id, username: usuario[0].username },
                 cookie.secret,
                 { expiresIn: cookie.expiracion }
             );
@@ -58,9 +65,9 @@ export class ControladorAuth {
             const { nombre, username, password } = req.body;
 
             // Comprobar si el nombre de usuario ya existe
-            const [existingUsers] = await modeloUsuario.findByUsername(username);
+            const existingUsers = await modeloUsuario.findByUsername(username);
 
-            if (existingUsers.length) {
+            if (existingUsers.length > 0) {
                 req.flash('error_msg', 'Este nombre de usuario ya está en uso!');
                 return res.redirect('/registro');
             }
@@ -69,7 +76,11 @@ export class ControladorAuth {
             const hashedPassword = await bcrypt.hash(password, 10);
 
             // Insertar el nuevo usuario en la base de datos
-            await modeloUsuario.createUser(name, username, hashedPassword);    
+            const resultado = await modeloUsuario.createUser(nombre, username, hashedPassword);    
+            if (resultado.affectedRows === 0) {
+                req.flash('error_msg', 'Error de base de datos. inténtelo más tarde');
+                return res.redirect('/registro');
+            }
 
             // Generar un token JWT
             const token = jwt.sign(
@@ -92,7 +103,6 @@ export class ControladorAuth {
     }
 
     async dashboard(req, res) {
-        console.log("Usuario logueado con éxito y navegando desde el dashboard")
         try {
             res.render(
                 path.resolve(VIEWS, "auth", "dashboard"), {
@@ -104,11 +114,31 @@ export class ControladorAuth {
         }
     }
 
-    async addProduct(req, res) {
+    // método GET que devuelve todos los productos, bajo login
+    async productosEdicion(req, res) {
+        let productos = await modeloProducto.getProductos(req.query);
         try {
             res.render(
-                path.resolve(VIEWS, "auth", "formAgregarProducto")
-            );
+                path.resolve(VIEWS, "publicas", "homepage"), {
+                    productos: productos, // la lista de productos
+                    user: req.user, // al ser vista con auth, se envía usuario
+                    vistaEdit: true, // visibiliza el botón de edición
+                    clearfilter: req.url // fija la url de limpiado de form de filtros
+                });
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    // método GET que devuelve la página para agregar un producto
+    async addProduct(req, res) {
+        let categorias = await modeloCategoria.getCategorias();
+        try {
+            res.render(
+                path.resolve(VIEWS, "auth", "formAgregarProducto"), {
+                    categorias: categorias,
+                    user: req.user
+                });
         } catch (error) {
             console.error('Error de servidor en "/dashboard/agregarproducto":', error);
             req.flash('error_msg', 'Error en el servidor, por favor intente de nuevo más tarde.');
@@ -116,13 +146,19 @@ export class ControladorAuth {
         }
     };
 
+    // método POST que recibe el formulario de nuevo producto
     async nuevoProducto(req, res) {
-        const { marca, nombre, precio, descripcion, stock, categoria } = req.body;
-
-        if (!req.files || !req.files.imagen) {
-            return res.status(400).send('No se subió ninguna imagen.');
+        // se valida el formulario en la ruta-auth.js con un middleware
+        const errores = validationResult(req);
+        if(!errores.isEmpty()){
+            console.log("Errores del formulario: ", errores)
+            req.flash('error_msg', 'Hay errores en el formulario. Revise sus datos y vuelva a intentar.');
+            return res.redirect('/dashboard/agregarproducto');
         }
 
+        // se continua con el agregado del producto
+        const { marca, nombre, precio, descripcion, stock, categoria } = req.body;
+        // se captura la imagen (si la hay)
         const imagen = req.files.imagen;
 
         try {
@@ -157,13 +193,37 @@ export class ControladorAuth {
             await connection.commit();
             connection.release();
 
-            res.status(201).send('Producto creado con éxito y la imagen subida.');
+            req.flash('success_msg', 'Producto agregado con éxito.');
+            return res.redirect('/dashboard/productos');
+
         } catch (error) {
             console.error('Error al crear el producto:', error);
             const connection = await pool.getConnection();
             await connection.rollback();
             connection.release();
-            res.status(500).send('Error al crear el producto.');
+
+            req.flash('error_msg', 'Error al crear el producto.');
+            return res.redirect('/dashboard/productos');
         }
-    }
+    };
+
+    // método GET que devuelve página con formulario para editar producto
+    async editProduct(req, res) {};
+
+    // método POST que procesa el formulario de edición de productos
+    async editarProducto(req, res) {};
+
+    async categorias(req, res) {};
+
+    async addCategory(req, res) {};
+
+    async nuevoCategoria(req, res) {};
+    
+    async logout(req, res) {
+        try {
+            res.clearCookie('token').redirect("/");
+        } catch (error) {
+            throw error;
+        }
+    };
 }
--- src/rutas/rutas-auth.js
+++ src/rutas/rutas-auth.js
@@ -4,6 +4,7 @@ import express from 'express';
 
 import { ControladorAuth } from '../controladores/controlador-auth.js';
 import { Autenticado } from '../middlewares/auth.js';
+import { validarProductoNuevo } from '../middlewares/validar-producto-nuevo.js';
 
 // instanciación del router de express
 const router = express.Router();
@@ -14,8 +15,14 @@ const controladorAuth = new ControladorAuth();
 router.post("/login", controladorAuth.login);
 router.post("/registro", controladorAuth.registro);
 router.get("/dashboard", Autenticado, controladorAuth.dashboard);
-router.get("/dashboard/agregarproducto", Autenticado, controladorAuth.addProduct);
-router.post("/dashboard/agregarproducto", Autenticado, controladorAuth.nuevoProducto);
+router.get("/dashboard/productos", Autenticado, controladorAuth.productosEdicion);
+router.get("/dashboard/agregarproducto", Autenticado, controladorAuth.addProduct); // envía formulario de nuevo producto
+router.get("/dashboard/editarproducto/:id", Autenticado, controladorAuth.editProduct); // envía formulario para editar un producto
+router.post("/dashboard/agregarproducto", Autenticado, validarProductoNuevo, controladorAuth.nuevoProducto); // procesa el formulario
+router.get("/dashboard/categorias", Autenticado, controladorAuth.categorias); //envía página con las categorías
+router.get("/dashboard/agregarcategoria", Autenticado, controladorAuth.addCategory); // envía formulario de nueva categoría
+router.post("/dashboard/agregarcategoria", Autenticado, controladorAuth.nuevoCategoria); // procesa formulario
+router.get("/logout", Autenticado, controladorAuth.logout);
 
 export {
     router
